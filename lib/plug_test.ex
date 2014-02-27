cf85bb70678e952f29233e2dc37e9bad99ef21ff
Andreas Amsenius <andreas@amsenius.se>
2014-02-27 16:26:53 +0100

    Assert webserver starts ok

--- lib/plug_test.ex
+++ lib/plug_test.ex
@@ -4,8 +4,8 @@ defmodule PlugTest do
   # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
   # for more information on OTP Applications
   def start(_type, _args) do
+    { :ok, _ } = Plug.Adapters.Cowboy.http PlugTest.AppRouter, [], port: 4000
     IO.puts "Running MyPlug with Cowboy on http://localhost:4000"
-    Plug.Adapters.Cowboy.http AppRouter, []
     PlugTest.Supervisor.start_link
   end
 end
