32fe6fca40d9a7f5681fb4a263c1ca936131cd2b
Andreas Amsenius <andreas@amsenius.se>
2014-03-11 01:19:11 +0100

    Print actual port number

--- lib/plug_test.ex
+++ lib/plug_test.ex
@@ -4,8 +4,11 @@ defmodule PlugTest do
   # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    { :ok, _ } = Plug.Adapters.Cowboy.http PlugTest.AppRouter, [], port: 4000
-    IO.puts "Running MyPlug with Cowboy on http://localhost:4000"
+    port = 4000
+    # TODO configure hostname
+    uri = "http://localhost:#{port}"
+    { :ok, _ } = Plug.Adapters.Cowboy.http(PlugTest.AppRouter, [], port: port)
+    IO.puts "Running MyPlug with Cowboy on #{uri}"
     PlugTest.Supervisor.start_link
   end
 end
