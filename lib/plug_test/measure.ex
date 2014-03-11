23d25dc29530c06fa5feafd02db08eaa67e3f454
Andreas Amsenius <andreas@amsenius.se>
2014-03-11 01:18:37 +0100

    Rename counter -> measure

--- lib/plug_test/measure.ex
+++ lib/plug_test/measure.ex
@@ -1,10 +1,11 @@
-defmodule PlugTest.Counters do
-  defrecord Counter, id: nil, name: nil, type: :counter, values: nil do
-    # type: counter | 2-state (on/off)
+defmodule PlugTest.Measures do
+  defrecord Measure, id: nil, name: nil, type: :nil, values: nil do
+    # type: event | 2-state (on/off)
     # numeric
     # multi-state
     # TODO: use cases of each
-    record_type id: String.t, name: String.t, type: :counter, values: [Value]
+    record_type id: String.t, name: String.t, type: :event|:duration,
+                               current_value: Value
   end
 
   defrecord Value, id: nil, timestamp: nil, value: nil do
@@ -13,32 +14,32 @@ defmodule PlugTest.Counters do
 
   # TODO JSON.encode escapes / -> \\/ for some reason, seems to look ok when
   # decoded though
-  @base_uri "http://..."
-  @counters "counters/"
+  @base_uri "http://localhost:4000"
+  @measures "measures/"
   @values "values/"
 
-  def read_counters do
-    File.ls!("#{cwd_path(@counters)}") |> Enum.map(&read_counter/1)
+  def read_measures do
+    File.ls!("#{cwd_path(@measures)}") |> Enum.map(&read_measure/1)
   end
 
-  def read_counter(counter_id, read_values? \\ false) do
-    values = if read_values?, do: read_values(counter_id)
-    case read_file("#{@counters}#{counter_id}") do
+  def read_measure(measure_id) do
+    case read_file("#{@measures}#{measure_id}") do
       {:error, _} -> nil
       {:ok, binary} ->
-        counter = counter_from_json(counter_id, binary)
-        counter.values(values)
+        measure = measure_from_json(measure_id, binary)
+        read_values(measure_id)
+        measure.current_value(value)
     end
   end
 
-  defp counter_from_json(id, binary) do
+  defp measure_from_json(id, binary) do
     json = JSON.decode!(binary)
-    Counter[id: id, name: Dict.fetch!(json, "name"),
+    Measure[id: id, name: Dict.fetch!(json, "name"),
                                type: Dict.fetch!(json, "type")]
   end
 
-  def read_values(counter_id) do
-    case read_file("#{@values}#{counter_id}") do
+  def read_values(measure_id) do
+    case read_file("#{@values}#{measure_id}") do
       {:error, _} -> nil
       {:ok, binary} -> values_from_json(binary)
     end
@@ -64,9 +65,9 @@ defmodule PlugTest.Counters do
     |> encode_json_values
   end
 
-  def update_values(values, counter_id) do
+  def update_values(values, measure_id) do
     # TODO version control
-    File.write! cwd_path("#{@values}#{counter_id}"), values_to_json(values)
+    File.write! cwd_path("#{@values}#{measure_id}"), values_to_json(values)
   end
 
   defp value_to_kv(Value[id: id, timestamp: timestamp, value: value]) do
@@ -77,20 +78,21 @@ defmodule PlugTest.Counters do
     JSON.encode! [values: kvs]
   end
 
-  def counters_to_json(counters) do
-    counters
-    |> Enum.map(&counter_to_kv/1)
-    |> encode_json_counters
+  def measures_to_json(measures) do
+    measures
+    |> Enum.map(&measure_to_kv/1)
+    |> encode_json_measures
   end
 
-  defp counter_to_kv(Counter[id: id, name: name, type: type]) do
-    [id: id, name: name, type: type]
+  defp measure_to_kv(Measure[id: id, name: name, type: type]) do
+    [id: id, name: name, type: type, links: [values: id]]
   end
 
-  defp encode_json_counters(kvs) do
-    JSON.encode! [links: ["counters.values":
-                          "#{@base_uri}/counters/{counters.id}/values"],
-                  counters: kvs]
+  defp encode_json_measures(kvs) do
+    [links: ["measures.values":
+             [href: "#{@base_uri}/measures/{measures.values}/values",
+              type: "values"]],
+     measures: kvs] |> JSON.encode!
   end
 
 
