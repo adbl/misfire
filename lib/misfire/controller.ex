86172292c47d053c0787270e8c82907c46dc8325
Andreas Amsenius <andreas@amsenius.se>
2014-05-05 07:04:00 +0200

    Unquote @ct_*, @msg_* from Controller

--- lib/misfire/controller.ex
+++ lib/misfire/controller.ex
@@ -1,14 +1,22 @@
 defmodule Misfire.Controller do
+  @ct_json "application/vnd.api+json"
+  @ct_text "text/plain"
+
+  @msg_201 "201 Created"
+  @msg_400 "400 Bad Request"            # check rfc 2616/jsonapi.org
+  @msg_404 "404 Not Found"
+  @msg_415 "415 Unsupported Media Type" # check rfc 2616/jsonapi.org
+
   defmacro __using__(_) do
     quote do
       import Misfire.Controller
-      @ct_json "application/vnd.api+json"
-      @ct_text "text/plain"
+      @ct_json unquote(@ct_json)
+      @ct_text unquote(@ct_text)
 
-      @msg_201 "201 Created"
-      @msg_400 "400 Bad Request"            # check rfc 2616/jsonapi.org
-      @msg_404 "404 Not Found"
-      @msg_415 "415 Unsupported Media Type" # check rfc 2616/jsonapi.org
+      @msg_201 unquote(@msg_201)
+      @msg_400 unquote(@msg_400)
+      @msg_404 unquote(@msg_404)
+      @msg_415 unquote(@msg_415)
     end
   end
 
